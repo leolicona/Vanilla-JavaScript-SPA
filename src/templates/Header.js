63765b4a5435684da014db5399d350e279b360a3
leolicona <leoncreactivo@gmail.com>
2021-12-26 13:59:47 -0600

    SPA with webpack, views, routing and API conect

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    const view = `
+        <nav class="Header-main">
+            <div class="Header-logo">
+                <h1><a href="/">
+                    100tifi.co
+                </a></h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#/about/">
+                    About
+                </a>
+            </div>
+        </nav>  
+    `;
+    return view;
+}
+
+export default Header;
